27df0b091daa2dcb84f725a94649e28857c1ebc4
Arkya Patwa <arkyapatwa333@gmail.com>
2024-02-20 22:34:57 +0530

    typewriter effect with normal theme

--- components/Home.tsx
+++ components/Home.tsx
@@ -2,12 +2,43 @@
 
 import React from "react";
 import { FloatingNavDemo } from "./Navbar";
+import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
+import { Button } from "./ui/button";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 const Home = () => {
+  const words = [
+    {
+      text: "A",
+    },
+    {
+      text: "Software",
+    },
+    {
+      text: "Developer.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+
   return (
     <>
-      <div id="home">
+      <div id="home" className="w-full h-screen flex items-center justify-center flex-col ">
         <FloatingNavDemo />
+        <div className="flex flex-col items-center justify-center h-[40rem]  ">
+          <h2 className="text-neutral-600 dark:text-neutral-200 text-lg sm:text-base lg:text-5xl font-bold">
+            Hey, I'm Arkya Patwa
+          </h2>
+          <TypewriterEffectSmooth words={words} />
+          <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
+            <Button>
+              Download Cv
+            </Button>
+            <Button variant={"secondary"} className="flex gap-2">
+              <GitHubLogoIcon />
+              Github
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   );
