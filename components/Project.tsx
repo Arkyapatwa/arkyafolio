672feb498ef7fc7addb92caa572396bed5c5d27f
Arkya Patwa <arkyapatwa333@gmail.com>
2024-02-23 23:55:10 +0530

    3d card added

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+"use client";
+import Card from "./Card";
+
 
 const Project = () => {
   return (
     <>
-      <div
-        id="projects"
-        className="w-full h-screen flex justify-center"
-
-      >
-        <div className="px-15 md:p-20 flex flex-col gap-10">
-          <h1 className="text-5xl font-semibold"><span className="text-blue-500">My</span> Works</h1>
-          <div className="border border-solid border-red-700 w-100 h-100 border-1 flex flex-col md:flex-row">
-
+      <div id="projects" className="w-full h-screen flex justify-center">
+        <div className="px-15 w-100 md:p-20 flex flex-col gap-10">
+          <h1 className="text-5xl font-semibold text-center">
+            <span className="text-blue-500">My</span> Works
+          </h1>
+          <div className="flex flex-col md:flex-row">
+            <Card />
           </div>
         </div>
       </div>
